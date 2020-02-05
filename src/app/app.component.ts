1357cb7f6a5de6b531d4e477fb465f109781a761
florian <florian.palabost@etu.univ-lyon1.fr>
2020-02-05 17:09:40 +0100

    update card file + clean code + use modulo to create more than 13 cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {FormBuilder} from '@angular/forms';
 import * as _ from 'underscore';
 
@@ -7,10 +7,10 @@ import * as _ from 'underscore';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'memory';
+export class AppComponent {
+  title = 'Mini-Memory';
   choiceNbCardsForm: any;
-  numberOfCardsChose: any;
+  numberOfCardsChosen: any;
   cards = [];
   nbClick = 0;
   nbPairs = 0;
@@ -22,33 +22,27 @@ export class AppComponent implements OnInit {
       numberCards: ['']
     });
   }
-  ngOnInit(): void {
-
-  }
 
   onSubmit(form: any) {
     this.nbClick = 0;
     this.cards = [];
-    this.numberOfCardsChose = form.numberCards;
+    this.numberOfCardsChosen = form.numberCards;
     this.nbPairs = form.numberCards / 2;
-
-
     let i = 1;
-    while (i <= this.numberOfCardsChose / 2) {
-      const newCard = {
-        'id': _.random(0, 100000),
-        'value': i,
-        'found': false,
-        'type': this.typeCards[Math.floor(Math.random() * this.typeCards.length)]
-      };
-      this.cards.push(newCard);
+    while (i <= this.numberOfCardsChosen / 2) {
+      const type = this.typeCards[Math.floor(Math.random() * this.typeCards.length)];
+      let newCard = createCard(i, type);
+      // todo avoid to have the same cards create
 
-      const newCard1 = {
-        'id': _.random(0, 100000),
-        'value': i,
-        'found': false,
-        'type': this.typeCards[Math.floor(Math.random() * this.typeCards.length)]
-      };
+      if (_.where(this.cards, {
+        'value': i % 12 + 1,
+        'type': type
+      }).length > 0 ) {
+        console.log('find ');
+        newCard = createCard(i, type);
+      }
+      this.cards.push(newCard);
+      const newCard1 = createCard(i, type);
       this.cards.push(newCard1);
       i++;
     }
@@ -58,14 +52,15 @@ export class AppComponent implements OnInit {
 
   }
 
+  /**
+   * handle check cards values on click
+   * @param card
+   */
   clickCard(card) {
-
     // rotate img
     const domCard = document.querySelector('#card_' + card.id);
     domCard.setAttribute('style', ' transform: rotateY(180deg);' );
 
-    console.debug(`card clicked : ${card.value}`);
-    // TODO check card is not the same that click previously
     switch (this.seen.length) {
       case 2:
         // not the same cards
@@ -73,11 +68,11 @@ export class AppComponent implements OnInit {
         reverseCard(domCard);
         break;
       case 1:
-        // cmp card clicked & card seen previously
-        if (this.seen[0].value === card.value && this.seen[0].id !== card.id) {
+        // cmp card clicked & card seen previously need to have same value & type but not same id
+        if (this.seen[0].value === card.value && this.seen[0].id !== card.id && this.seen[0].type === card.type) {
           // same cards so update found of the 2 cards
           _.each(this.cards, (carte) => {
-            if (carte.value === this.seen[0].value) {
+            if (carte.value === this.seen[0].value && carte.type === this.seen[0].type) {
               carte.found = true;
               console.log(carte);
             }
@@ -109,9 +104,25 @@ export class AppComponent implements OnInit {
 
 }
 
+/**
+ * apply rotation style to the card pass in param
+ * @param domCard
+ */
 function reverseCard(domCard) {
   setTimeout(() => {
     domCard.setAttribute('style', ' transform: rotateY(0deg);' );
 
   }, 1000);
+}
+
+/**
+ * return a new card to add to the cards list
+ */
+function createCard(i, type) {
+  return {
+    'id': _.random(0, 100000),
+    'value': i % 12 + 1,
+    'found': false,
+    'type': type
+  };
 }
